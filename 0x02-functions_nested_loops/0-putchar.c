d7b5ec338321ed698e2b927152d7c50b743352d3
zero7four7 <edmundmajanga@outlook.com>
2023-08-22 13:19:32 -0700

    Task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,15 +8,15 @@
 
 int main(void)
 {
-	char c[] = "main";
+	char c[10] = "_putchar";
 
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(c[i]);
 	}
-	_putchar('\n');
+	_putchar(10');
 	return (0);
 }
 

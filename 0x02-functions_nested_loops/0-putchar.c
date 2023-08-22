31d8361e73ca451d8b5375fb682497fc6351675e
zero7four7 <edmundmajanga@outlook.com>
2023-08-22 13:07:00 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char school[10] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(School[i]);
+	}
+	_putchar(10);
+	return (0);
+}
+

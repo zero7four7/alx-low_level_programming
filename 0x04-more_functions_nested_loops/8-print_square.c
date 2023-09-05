762606067064ee26859287f678dee59b4200900f
zero7four7 <edmundmajanga@outlook.com>
2023-09-04 17:56:01 -0700

    Changes

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,20 +10,19 @@ void print_square(int size)
 {
 	int x, y;
 
-	if (size <= 0)
-	{
-		_putchar('\n')
-	}
-	else
+	if (size > 0)
 	{
 		for (x = 1; x <= size; x++)
 		{
-			_putchar('#');
-			for (y = 2; y <= size; y++)
+			for (y = 1; y <= size; y++)
 			{
 				_putchar('#');
 			}
 			_putchar('\n');
 		}
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }

2f41f99bc76b8eadb7e5ad3a1b973e54ef79425b
zero7four7 <edmundmajanga@outlook.com>
2023-09-05 07:46:24 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test function  prints integer  positive or negative
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}

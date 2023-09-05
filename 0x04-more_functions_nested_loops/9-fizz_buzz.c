3a71b2e6ef158583160082dbb927ceca510e6939
zero7four7 <edmundmajanga@outlook.com>
2023-09-04 17:43:53 -0700

    Task 10

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: ALways 0.
+ */
+
+int main (void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0 || x % 5 == 0)
+		{
+			if ( x % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (x % 5 == 0)
+			{
+				printf("Buzz");
+			}
+		}
+		else
+		{
+			printf("%d", x);
+		}
+		if (x != 100)
+		{
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

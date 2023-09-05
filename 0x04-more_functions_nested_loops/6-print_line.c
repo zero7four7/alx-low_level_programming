798891d0254a3b6bcddae2d5a4b92b5ee0ce645d
zero7four7 <edmundmajanga@outlook.com>
2023-09-04 17:26:00 -0700

    Function drwas straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - function drwas straight line in the terminal
+ *
+ * Return: straigh line.
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x <= n; x++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}

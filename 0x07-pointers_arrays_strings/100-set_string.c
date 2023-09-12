6469be9078c7ab5bf7566ad5d4b9e57deaa126ad
zero7four7 <edmundmajanga@outlook.com>
2023-09-12 16:30:36 -0700

    0x07-pointer_arrays_strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * set_string - sets the value of a pointer to a char.
+ * @s: double pointer to be copied
+ * @to: where s gets set to
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

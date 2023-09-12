656d0f8395133fbab6d62735397160ea15045c05
zero7four7 <edmundmajanga@outlook.com>
2023-09-12 15:54:37 -0700

    task change

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,8 +7,7 @@ int main(void)
   int *p;
 
   a[2] = 1024;
-  p = &n;
- 
+  p = &n; 
   /*
    * write your line of code here...
    * Remember:
@@ -16,13 +15,9 @@ int main(void)
    * - you are not allowed to modify p
    * - only one statement
    * - you are not allowed to code anything else than this line of code
-   */
- 
-  *(p + 5) = 98;
- 
+   */ 
+  *(p + 5) = 98; 
   /* ...so that this prints 98\n */
- 
-  printf("a[2] = %d\n", a[2]);
- 
+  printf("a[2] = %d\n", a[2]); 
   return (0);
 }

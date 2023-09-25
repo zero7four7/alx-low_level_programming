ca5d15d91d9da601aa7dd874ddbc2ba4a45d3964
zero7four7 <edmundmajanga@outlook.com>
2023-09-25 01:00:04 -0700

    fixed typo

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "lists.h"
-#include <stddef.h>
 
 /**
  * free_list - Frees a linked list.
@@ -11,9 +10,21 @@
 
 void free_list(list_t *head)
 {
-	if (head == NULL)
-		return;
-	free_list(head->next);
-	free(head->str);
-	free(head);
+	list_t *actual_node;
+	list_t *next_node;
+
+	if (head)
+	{
+		actual_node = head;
+		next_node = head->next;
+		while (next_node)
+		{
+			free(actual_node->str);
+			free(actual_node);
+			actual_node = next_node;
+			next_node = next_node->next;
+		}
+		free(actual_node->str);
+		free(actual_node);
+	}
 }

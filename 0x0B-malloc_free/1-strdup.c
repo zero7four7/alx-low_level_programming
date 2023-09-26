2752d479a0b49214898cf02002fb883f5d8149e5
zero7four7 <edmundmajanga@outlook.com>
2023-09-26 16:09:00 -0700

    updates

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -24,11 +24,12 @@ char *_strdup(char *str)
 	{
 		for (i = 0; str[i] != '\0'; i++)
 			strnew[i] = str[i];
-	} else
+	}
+	else
 	{
 		return (NULL);
 	}
 	strnew[i] = '\0';
-	
+
 	return (strnew);
 }

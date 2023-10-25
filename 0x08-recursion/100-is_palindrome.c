2724a934edc26f58dce08bb0d511c8fe847ba3ee
zero7four7 <edmundmajanga@outlook.com>
2023-10-25 14:05:19 -0700

    0x08-recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * longitud - Function returns the power of number
- * @s: String
- *
- * Return: Length of a string
- */
+*longitud - function that returns the power of number
+*@s: string
+*
+*Return: length of a string
+*/
 
 int longitud(char *s)
 {
@@ -20,18 +20,17 @@ int longitud(char *s)
 }
 
 /**
- * comparar - function returns power of number
- * @i: original number
- * @l: counter to compare multiplication
- * @s: string
- *
- * Return: square root
+ *comparar - function that returns the power of number
+ *@i: original number
+ *@l: counter to compare multiplication
+ *@s: string
+ *Return: square root
  */
 
 int comparar(int i, int l, char *s)
 {
 	if (i >= l)
-		return(1);
+		return (1);
 	else if (s[i] == s[l])
 		return (comparar(i + 1, l - 1, s));
 	else
@@ -39,10 +38,9 @@ int comparar(int i, int l, char *s)
 }
 
 /**
- * is_palindrome - return 1 if number is prime
- * @s: number to evaluate
- *
- * Return: 1 or 0.
+ *is_palindrome - return 1 if is number prime
+ *@s: number to evaluate
+ *Return: 1 or 0
  */
 
 int is_palindrome(char *s)
@@ -50,7 +48,6 @@ int is_palindrome(char *s)
 	int l = longitud(s);
 	int i = 0;
 
-	return (comparar(i, l - 1. s));
+	return (comparar(i, l - 1, s));
 }
-
 

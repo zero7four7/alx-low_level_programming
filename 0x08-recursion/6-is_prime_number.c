826acfd26bb7bada95c732179cae1e1477dbcb57
zero7four7 <edmundmajanga@outlook.com>
2023-10-25 13:50:53 -0700

    0x08-recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -20,3 +20,18 @@ int comparar(int n, int y)
 	}
 	return (count);
 }
+
+/**
+ * is_prime_number - return 1 if number is prime
+ * @n: number to evaluate
+ *
+ * Return: 1 or 0.
+ */
+
+int is_prime_number(int n)
+{
+	if (comparar(n, 1) == 2)
+		return (1);
+	else
+		return (0);
+}
